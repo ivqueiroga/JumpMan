3a639a9a2ca7656676b5c47d48491b8309ae0277
ivqueiroga <ivanqueirogap@gmail.com>
2023-01-06 14:34:23 -0300

    Added Color Palette define database

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_GFX.h>
 #include <gamma.h>
+#include <ColorPalette.h>
 
 // defines and constants
 #define PIN     18              // data pin on ESP-32 board
@@ -13,25 +14,25 @@
 const int REFRESH = 65;         // refresh rate
 
 
-// color pallet
-#define Black           0x0000      /*   0,   0,   0 */
-#define Navy            0x000F      /*   0,   0, 128 */
-#define DarkGreen       0x03E0      /*   0, 128,   0 */
-#define DarkCyan        0x03EF      /*   0, 128, 128 */
-#define Maroon          0x7800      /* 128,   0,   0 */
-#define Purple          0x780F      /* 128,   0, 128 */
-#define Olive           0x7BE0      /* 128, 128,   0 */
-#define LightGrey       0xC618      /* 192, 192, 192 */
-#define DarkGrey        0x7BEF      /* 128, 128, 128 */
-#define Blue            0x001F      /*   0,   0, 255 */
-#define Green           0x07E0      /*   0, 255,   0 */
-#define Cyan            0x07FF      /*   0, 255, 255 */
-#define Red             0xF800      /* 255,   0,   0 */
-#define Magenta         0xF81F      /* 255,   0, 255 */
-#define Yellow          0xFFE0      /* 255, 255,   0 */
-#define White           0xFFFF      /* 255, 255, 255 */
-#define Orange          0xFD20      /* 255, 165,   0 */
-#define GreenYellow     0xAFE5      /* 173, 255,  47 */
+// // color palette
+// #define Black           0x0000      /*   0,   0,   0 */
+// #define Navy            0x000F      /*   0,   0, 128 */
+// #define DarkGreen       0x03E0      /*   0, 128,   0 */
+// #define DarkCyan        0x03EF      /*   0, 128, 128 */
+// #define Maroon          0x7800      /* 128,   0,   0 */
+// #define Purple          0x780F      /* 128,   0, 128 */
+// #define Olive           0x7BE0      /* 128, 128,   0 */
+// #define LightGrey       0xC618      /* 192, 192, 192 */
+// #define DarkGrey        0x7BEF      /* 128, 128, 128 */
+// #define Blue            0x001F      /*   0,   0, 255 */
+// #define Green           0x07E0      /*   0, 255,   0 */
+// #define Cyan            0x07FF      /*   0, 255, 255 */
+// #define Red             0xF800      /* 255,   0,   0 */
+// #define Magenta         0xF81F      /* 255,   0, 255 */
+// #define Yellow          0xFFE0      /* 255, 255,   0 */
+// #define White           0xFFFF      /* 255, 255, 255 */
+// #define Orange          0xFD20      /* 255, 165,   0 */
+// #define GreenYellow     0xAFE5      /* 173, 255,  47 */
 
 // variables that will change
 bool jumpLastState,gameOver,launch,jumpSide;
