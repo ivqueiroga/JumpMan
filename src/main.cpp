8069aaeb15431b3972e118920916127ecdee28e2
ivqueiroga <ivanqueirogap@gmail.com>
2023-01-17 13:29:49 -0300

    BLE Jump Function

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <Adafruit_GFX.h>
 #include <gamma.h>
 #include <ColorPalette.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
 
 // defines and constants
 #define PIN     18              // data pin on ESP-32 board
@@ -11,14 +14,14 @@
 #define NUMPIXELS mW*mH         // matrix length
 #define BRIGHTNESS 40           // max 255, 32 is a good default and more current safe
 #define gravity 1               // gravity applied to fall effect
+#define SERVICE_UUID   "13e0ba4e-9678-11ed-a1eb-0242ac120002"
+#define CHARACTERISTIC_UUID "311ffe9e-9678-11ed-a1eb-0242ac120002"
 const int REFRESH = 65;         // refresh rate
 
 // variables that will change
-bool gameOver;
+bool gameOver, lastJumpState;
 int pebbleX,lifeBar,jumpCount;
 unsigned long previousTimer = 0;
-
-
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(mW, mH, PIN,
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
@@ -32,7 +35,7 @@ class Pixel {
 
   public:
     int pX, pY;
-    float velocity=.5;
+    float velocity=0;
 
     void set_values (int x, int y, int col)
       { pX=x; pY=y; color=col; }
@@ -74,12 +77,40 @@ Pixel player1;
 Pixel pebble1;
 Background bg;
 
+// Helper functions
+
+BLEServer *pServer;
+BLEService *pService;
+BLECharacteristic *pCharacteristic;
+
 void setup() {
   // Initialize serial and wait for port to open:
   Serial.begin(115200);
   // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
-  delay(1500); 
+  delay(1500);
 
+  BLEDevice::init("PixelFrame");
+  pServer = BLEDevice::createServer();
+  pService = pServer->createService(SERVICE_UUID);
+  pCharacteristic = pService->createCharacteristic(
+    CHARACTERISTIC_UUID,
+    BLECharacteristic::PROPERTY_READ |
+    BLECharacteristic::PROPERTY_WRITE
+  );
+  pCharacteristic->setValue("1");
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06); // functions that help with iPhone connections issues
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+
+  Serial.println("Characteristic defined! Now you can read it in the Client!");
+  
+
+  lastJumpState = 0;
   matrix.begin();
   matrix.setBrightness(BRIGHTNESS);
   bg.set_values(Cyan, DarkGreen, Maroon);
@@ -100,12 +131,14 @@ void Draw() {
 }
 
 void Input() {
-  if (jumpCount > 0){
-    if (pebble1.pX == 6)
-    {
+  std::string value = pCharacteristic->getValue();
+  bool val = std::stoi(value.c_str());
+  if (lastJumpState != val) {
+    lastJumpState = val;
+    if (val == 0) {
+      Serial.println(val);
       player1.velocity = -2;
-      // jumpCount--; // life loss and gameOver simulation after jump count ends
-    }
+    };
   }
 }
 
@@ -113,14 +146,14 @@ void Logic() {
   if(--pebbleX >= 0) {
       pebble1.pX = pebbleX;
     } else pebbleX = mW;
-  if (player1.pX == pebble1.pX && player1.pY == pebble1.pY){
-  lifeBar--;
-  }
-  if (lifeBar < 0) gameOver = true;
+  // if (player1.pX == pebble1.pX && player1.pY == pebble1.pY){
+  // lifeBar--;
+  // }
+  // if (lifeBar < 0) gameOver = true;
 }
 
 void loop() {
-while (!gameOver) {
+  while (!gameOver) {
   unsigned long currentTimer = millis();
     if (currentTimer - previousTimer >= REFRESH)
     {
